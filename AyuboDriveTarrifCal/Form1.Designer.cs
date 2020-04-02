9ca9b20e9f2f5b7d8d6d4b772b9d17cca76f0f14
Dileepa Samarawickrama <dansamarawicky@outlook.com>
2020-04-03 02:25:20 +0530

    Untracked file added

--- AyuboDriveTarrifCal/Form1.Designer.cs
+++ AyuboDriveTarrifCal/Form1.Designer.cs
@@ -28,13 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panelLeft = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelControls = new System.Windows.Forms.Panel();
+            this.panelLeft.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelLeft
+            // 
+            this.panelLeft.Controls.Add(this.button4);
+            this.panelLeft.Controls.Add(this.button3);
+            this.panelLeft.Controls.Add(this.button2);
+            this.panelLeft.Controls.Add(this.button1);
+            this.panelLeft.Location = new System.Drawing.Point(12, 12);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(143, 426);
+            this.panelLeft.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Montserrat Alternates", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(0, 198);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(143, 60);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Manage";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Montserrat Alternates", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(0, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 60);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Completed Trips";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Montserrat Alternates", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(0, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 60);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Tarrifs Calculator";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Montserrat Alternates", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Hire / Rent";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panelControls
+            // 
+            this.panelControls.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelControls.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panelControls.Location = new System.Drawing.Point(161, 12);
+            this.panelControls.Name = "panelControls";
+            this.panelControls.Size = new System.Drawing.Size(627, 426);
+            this.panelControls.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelControls);
+            this.Controls.Add(this.panelLeft);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panelLeft.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelLeft;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panelControls;
     }
 }
 
--- AyuboDriveTarrifCal/Form1.cs
+++ AyuboDriveTarrifCal/Form1.cs
@@ -15,6 +15,42 @@ namespace AyuboDriveTarrifCal
         public Form1()
         {
             InitializeComponent();
+            userControls.UC_HireRent ucHireRent = new userControls.UC_HireRent();
+            AddControlsToPanel(ucHireRent);
         }
+
+
+        private void AddControlsToPanel(Control c)
+        {
+            c.Dock = DockStyle.Fill;
+            panelControls.Controls.Clear();
+            panelControls.Controls.Add(c);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            userControls.UC_HireRent ucHireRent = new userControls.UC_HireRent();
+            AddControlsToPanel(ucHireRent);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            userControls.UC_TarrifCal ucHireRent = new userControls.UC_TarrifCal();
+            AddControlsToPanel(ucHireRent);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            userControls.UC_CompletedTrips ucHireRent = new userControls.UC_CompletedTrips();
+            AddControlsToPanel(ucHireRent);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            userControls.UC_Manage ucHireRent = new userControls.UC_Manage();
+            AddControlsToPanel(ucHireRent);
+        }
+
+
     }
 }
